1fa36a638aa2adea03e585eb12a7c27a47dd3655
Shary Batista <you@example.com>
2025-06-22 17:28:15 -0400

    added new print line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("This project is using git.");
+        System.out.println("Pushing to Github...!");
     }
 }
